755847c790154310865a39d6164191d37563ea52
andrenogueiragbi <andrepereiragbi@gmail.com>
2022-06-04 22:30:08 -0300

    debugando heroku

--- main.py
+++ main.py
@@ -56,8 +56,8 @@ def search(type):
         return redirect(url_for(f'search',type))
 
     #SE O USUÁRIO NÃO TIVER LOGADO, ENVIA PARA O TELA LOGIN
-    #return redirect(url_for("login")) 
-    return render_template('debug.html',session=session)
+    return redirect(url_for("login")) 
+
 
 #ROTA DE FAZER LOGIN
 @app.route("/login",methods=["GET", "POST"])
